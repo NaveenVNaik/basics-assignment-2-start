db098c42c08dd2825896f12ebac694b60133f275
Naveen <naveennaikhnr@gmail.com>
2018-05-16 23:38:40 +0530

    solution has been added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,4 +6,13 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  userName: string = '';
+  userNameStatus: boolean = false;
+
+  getUserNameStatus = () => {
+    if(this.userName.length === 0)
+      return false;
+    return true;
+  }
+  
 }
